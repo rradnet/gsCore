4beca8adffd21da56ef227b3073ec96af084f23b
Greg Meess <greg@sutromachine.com>
2019-11-12 15:58:28 -0800

    Example of extending using the new IEngine interface and MEF discovery

---
+++ clientTest/ExtendCoreClassesDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using g3;
+using gs;
+using gs.interfaces;
+
+namespace clientTest
+{
+    public class SettingsExtended : GenericRepRapSettings
+    {
+        public double NewSetting { get; set; } = 10;
+    }
+
+    public class PrintGeneratorExtended : SingleMaterialFFFPrintGenerator
+    {
+
+    }
+
+    public class SettingsManagerExtended : SettingsManager<SettingsExtended>
+    {
+        public override List<SettingsExtended> FactorySettings { 
+            get {
+                var result = new List<SettingsExtended>();
+                var a = new SettingsExtended();
+                var b = new SettingsExtended();
+
+                a.BaseMachine.ManufacturerName = "ExtenderCo";
+                b.BaseMachine.ManufacturerName = "ExtenderCo";
+
+                a.BaseMachine.ModelIdentifier = "Generic";
+                b.BaseMachine.ManufacturerName = "Plus";
+
+                result.Add(a);
+                result.Add(b);
+                return result;
+            }
+        }
+
+        public override IUserSettingCollection<SettingsExtended> UserSettings =>
+            new UserSettingsExtended<SettingsExtended>();
+    }
+
+    [Export(typeof(IEngine))]
+    [ExportMetadata("Name", "ext")]
+    [ExportMetadata("Description", "Awesome extended engine.")]
+    public class EngineExtended : Engine<SettingsExtended>
+    {
+        public override IGenerator<SettingsExtended> Generator => 
+            new SinglePartGenerator<PrintGeneratorExtended, SettingsExtended>();
+
+        public override ISettingsManager<SettingsExtended> SettingsManager =>             
+            new SettingsManagerExtended();
+
+        public override List<IVisualizer> Visualizers => null;
+    }
+}
---
+++ clientTest/UserSettingsExtended.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using gs;
+using gs.interfaces;
+
+namespace clientTest
+{
+    public class UserSettingsExtended<TSettings> : UserSettingsFFF<TSettings> where TSettings : SettingsExtended
+    {
+        public static UserSettingGroup GroupExtended = new UserSettingGroup(() => UserSettingsExtendedTranslations.GroupExtended);
+
+        public UserSetting<SettingsExtended, double> NewSetting = new UserSetting<SettingsExtended, double>(
+            () => UserSettingsExtendedTranslations.NewSetting_Name,
+            () => UserSettingsExtendedTranslations.NewSetting_Description,
+            GroupExtended,
+            (settings) => settings.NewSetting,
+            (settings, val) => settings.NewSetting = val,
+            UserSettingNumericValidations<double>.ValidateMin(0, ValidationResult.Level.Error));
+    }
+}
