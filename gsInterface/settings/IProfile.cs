63a116776d374a1ee95bc6d4e2391f3b2802c6be
Greg Meess <greg@sutromachine.com>
2019-12-04 21:23:07 -0800

    Extend IProfile properties

--- gsInterface/settings/IProfile.cs
+++ gsInterface/settings/IProfile.cs
@@ -2,13 +2,16 @@
 {
     public interface IProfile
     {
-        string ManufacturerName { get; }
-        string ModelIdentifier { get; }
-        string ProfileName { get; }
+        string ManufacturerName { get; set; }
+        string ModelIdentifier { get; set; }
+        string ProfileName { get; set; }
 
         double MachineBedSizeXMM { get; }
         double MachineBedSizeYMM { get; }
         double MachineBedSizeZMM { get; }
+
+        double MachineBedOriginFactorX { get; }
+        double MachineBedOriginFactorY { get; }
 
         IProfile Clone();
     }
